dff76f01e871d873c2fb3a0aec697b3103b50fed
mHayaka3a <mih.hayakawa@opt.ne.jp>
2024-04-12 17:20:16 +0900

    px→rem、ヘッダースクロール表示、アドバイザー名表示修正

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -5,6 +5,21 @@ const text =
     "パンツはスタイルが良く見えるハイウエストで細身のシルエットです。それぞれセットで着ても別で着ても可愛いです。テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト";
 
 $(function () {
+    //スクロール時headerの表示切り替え
+    const headerHeight = $(".header").height();
+    const header = $(".header");
+
+    $(window).scroll(function (e) {
+        const currentPosition = $(this).scrollTop();
+
+        if (currentPosition > headerHeight) {
+            header.css("opacity", "0");
+        } else {
+            header.css("opacity", "1");
+        }
+    });
+
+    //モーダルを開く
     $(".js_open").on("click", function () {
         const clickedSrc = $(this).find(".styling_list_card_img").attr("src");
         $(".js_modal_img").attr("src", clickedSrc);
