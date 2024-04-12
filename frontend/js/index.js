63e5650185045b7e3807e899d87d5de6e4797dde
mHayaka3a <mih.hayakawa@opt.ne.jp>
2024-04-12 13:45:29 +0900

    モーダル機能追加、PC用CSS追加

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,1 +1,20 @@
 import "../scss/style.scss";
+import $ from "../npm/node_modules/jquery";
+
+const text =
+    "パンツはスタイルが良く見えるハイウエストで細身のシルエットです。それぞれセットで着ても別で着ても可愛いです。テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト";
+
+$(function () {
+    $(".js_open").on("click", function () {
+        const clickedSrc = $(this).find(".styling_list_card_img").attr("src");
+        $(".js_modal_img").attr("src", clickedSrc);
+        $(".js_modal_text").text(text);
+        $(".js_modal").css("display", "unset");
+    });
+    // モーダルを閉じる
+    $(".js_modal").on("click", function (e) {
+        if ($(e.target).closest(".styling_list_modal").length === 0) {
+            $(".js_modal").css("display", "none");
+        }
+    });
+});
